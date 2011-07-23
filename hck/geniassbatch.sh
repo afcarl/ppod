5ea668733de90276aaf89bd0295eb3ea06ccc602
Pontus Stenetorp <pontus@stenetorp.se>
2011-07-23 21:18:06 +0900

    GeniaSS Batch Script

---
+++ hck/geniassbatch.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Batch script for sentence splitting with GeniaSS. If you don't have
+# the patched version of GeniaSS that supports `-q` remove it and enjoy
+# model loading messages on `stderr`.
+#
+# Patched GeniaSS:
+#
+#    https://github.com/ninjin/geniass
+#
+# Example:
+#
+#     find . -name '*.txt' | parallel -m -j ${ADJUST_THIS} geniassbatch.sh
+#
+# Author:   Pontus Stenetorp    <pontus stenetorp se>
+# Version:  2011-07-16
+
+# Adjust these according to your environment
+GENIA_SS_DIR=~/git/geniass/
+SUPPORTING_DIR=~/git/bionlp_st_2011_supporting/
+GENIA_SS_POSTPROC=${SUPPORTING_DIR}/tools/geniass-postproc.pl
+
+# Suffix to give the sentence splitted version of the files
+SS_SUFFIX=ss
+
+# Iterate over all paths given as args and sentence split the files
+for ARG in $@
+do
+    INPUT_FILE=`readlink -f ${ARG}`
+    OUTPUT_FILE=${INPUT_FILE}.${SS_SUFFIX}
+
+    # GeniaSS needs a stupid temporary file for output...
+    SS_TMP=`mktemp`
+
+    (
+    cd ${GENIA_SS_DIR} &&
+        ./geniass -q ${INPUT_FILE} ${SS_TMP} &&
+        ${GENIA_SS_POSTPROC} ${SS_TMP} > ${OUTPUT_FILE}
+    )
+
+    rm -f ${SS_TMP}
+done
