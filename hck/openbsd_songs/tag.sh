2fcc2fb3820ae7b866762c81e29586bee8a689ad
Pontus Stenetorp <pontus@stenetorp.se>
2012-05-25 11:26:11 +0900

    String quoting fix.

--- hck/openbsd_songs/tag.sh
+++ hck/openbsd_songs/tag.sh
@@ -14,12 +14,12 @@ TAG_LIST=tag.list
 for ogg_file in `ls *.ogg`
 do
     title=`grep "^${ogg_file}" ${TAG_LIST} | cut -f 2`
-    if [ -z ${title} ]
+    if [ -z "${title}" ]
     then
         echo "WARNING: no entry in ${TAG_LIST} for ${ogg_file}, skipping" 1>&2
         continue
     fi
     lltag --yes --clear ${ogg_file} > /dev/null
     lltag --yes --ARTIST 'OpenBSD' --ALBUM 'OpenBSD' --TITLE "${title}" \
-        ${ogg_file} > /dev/null
+        "${ogg_file}" > /dev/null
 done
