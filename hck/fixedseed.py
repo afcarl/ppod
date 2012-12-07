3388125bff9cfdf8e5fcb7ef3ca9a3d00ef18eeb
Pontus Stenetorp <pontus@stenetorp.se>
2012-12-07 18:24:45 +0900

    FixedSeed, now for numpy too

--- hck/fixedseed.py
+++ hck/fixedseed.py
@@ -14,33 +14,56 @@ Author:     Pontus Stenetorp    <pontus stenetorp se>
 Version:    2011-08-24
 '''
 
-from random import getstate, seed, setstate
-
 class FixedSeed(object):
-    def __init__(self, seed):
-        self.seed = seed
+    def __init__(self, _seed, numpy=False):
+        self.seed = _seed
         self.state = None
+        if not numpy:
+            from random import getstate, seed, setstate
+            self.__seed = seed
+            self.__getstate = getstate
+            self.__setstate = setstate
+        else:
+            from numpy.random import get_state, seed, set_state
+            self.__seed = seed
+            self.__getstate = get_state
+            self.__setstate = set_state
 
     def __enter__(self):
-        self.state = getstate()
-        seed(self.seed)
+        self.state = self.__getstate()
+        self.__seed(self.seed)
         return self
     
     def __exit__(self, type, value, traceback):
-        setstate(self.state)
+        self.__setstate(self.state)
 
 # Minor test code
 if __name__ == '__main__':
-    from random import random
+    from sys import stderr
 
-    with FixedSeed(4711):
-        a = [random() for _ in xrange(17)]
-    after_a = random()
+    def _test(numpy=False):
+        if not numpy:
+            from random import random
+        else:
+            from numpy.random import random
 
-    with FixedSeed(4711):
-        b = [random() for _ in xrange(17)]
-    after_b = random()
+        with FixedSeed(4711, numpy=numpy):
+            a = [random() for _ in xrange(17)]
+        after_a = random()
 
-    assert a == b
-    # Should fail in most cases
-    assert after_a != after_b
+        with FixedSeed(4711, numpy=numpy):
+            b = [random() for _ in xrange(17)]
+        after_b = random()
+
+        assert a == b
+        # Should fail in most cases
+        assert after_a != after_b
+
+    # Test the standard Python implementation
+    _test()
+
+    # Test the numpy implementation
+    try:
+        _test(numpy=True)
+    except ImportError:
+        print >> stderr, "WARNING: numpy import failure, skipping numpy tests"
